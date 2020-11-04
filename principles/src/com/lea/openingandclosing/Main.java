0b638e82e0b154df5e29a69bc3441f4df4098eb6
liam <380639608@qq.com>
2020-11-04 10:32:31 +0800

    开闭原则

---
+++ principles/src/com/lea/openingandclosing/Main.java
@@ -0,0 +1,11 @@
+package com.lea.openingandclosing;
+
+/**
+ * @author lzc
+ * @create 2020-11-4 10:22
+ * 开闭原则是总纲，概念很大，目前我不是很理解，所以 只写下定义
+ * 开闭原则：
+ *  一个软件实体如类、模块和函数，应该对扩展开放，对修改关闭
+ */
+public class Main {
+}
