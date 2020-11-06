2fa5e35e763274abf7f62e52405a8fd1a10663e4
liam <380639608@qq.com>
2020-11-06 16:21:58 +0800

    工厂模式的扩展

---
+++ patterns/src/com/lea/pattern_2/subsingleton/Singleton.java
@@ -0,0 +1,10 @@
+package com.lea.pattern_2.subsingleton;
+
+/**
+ * @author lzc
+ * @create 2020-11-6 15:54
+ */
+public class Singleton {
+    // 私有化 构造器
+    private Singleton() {};
+}
---
+++ patterns/src/com/lea/pattern_2/subsingleton/SingletonFactory.java
@@ -0,0 +1,32 @@
+package com.lea.pattern_2.subsingleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author lzc
+ * @create 2020-11-6 15:55
+ *
+ * 通过工厂模式 创建对象
+ *
+ */
+public class SingletonFactory {
+
+    private static Singleton instance;
+
+    static {
+        // 通过反射获取对象
+        try {
+            Class<?> clazz = Class.forName(Singleton.class.getName());
+            Constructor constructor = clazz.getDeclaredConstructor();// 获取无参构造器
+            constructor.setAccessible(true);  // 设置无参构造器可以访问
+            instance = (Singleton) constructor.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Singleton getSingleton() {
+        return instance;
+    }
+}
