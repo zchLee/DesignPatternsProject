bbb1dc2d485064cfa15fee54a2d992d1b2c4c4f3
liam <380639608@qq.com>
2020-11-06 16:20:59 +0800

    初始工厂模式

---
+++ patterns/src/com/lea/pattern_2/factory/ConcreteCreator.java
@@ -0,0 +1,20 @@
+package com.lea.pattern_2.factory;
+
+/**
+ * @author lzc
+ * @create 2020-11-6 10:57
+ * 提供具体实现 创建工厂，生产产品
+ */
+public class ConcreteCreator extends Creator {
+
+    @Override
+    public <T extends Product> T createProduct(Class<T> c) {
+        T t = null;
+        try {
+            t = c.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+}
---
+++ patterns/src/com/lea/pattern_2/factory/ConcreteProduct.java
@@ -0,0 +1,13 @@
+package com.lea.pattern_2.factory;
+
+/**
+ * @author lzc
+ * @create 2020-11-6 10:52
+ * 提供一个具体产品
+ */
+public class ConcreteProduct extends Product {
+    @Override
+    public void method() {
+        System.out.println("---------------具体产品1展示-----------");
+    }
+}
