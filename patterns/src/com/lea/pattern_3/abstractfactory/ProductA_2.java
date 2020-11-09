1a69ef8e4aa8812fa82eb9899161491f134a7ed7
liam <380639608@qq.com>
2020-11-09 11:33:41 +0800

    抽象工厂

---
+++ patterns/src/com/lea/pattern_3/abstractfactory/ProductA_2.java
@@ -0,0 +1,12 @@
+package com.lea.pattern_3.abstractfactory;
+
+/**
+ * @author lzc
+ * @create 2020-11-9 10:46
+ */
+public class ProductA_2 extends AbstractProductA {
+    @Override
+    public void doSomething() {
+        System.out.println("A产品 2 的实现方法");
+    }
+}
---
+++ patterns/src/com/lea/pattern_3/abstractfactory/ProductB_1.java
@@ -0,0 +1,13 @@
+package com.lea.pattern_3.abstractfactory;
+
+/**
+ * @author lzc
+ * @create 2020-11-9 10:50
+ * 产品B 1 的具体实现
+ */
+public class ProductB_1 extends AbstractProductB {
+    @Override
+    public void doSomethingMethod() {
+        System.out.println("产品B 1 的具体实现");
+    }
+}
