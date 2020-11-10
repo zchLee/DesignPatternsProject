0ceacbb70812f6a3963343ac27a9b28d6f3a53f3
liam <380639608@qq.com>
2020-11-10 16:12:11 +0800

    单例模式

--- patterns/src/com/lea/pattern_1/Main.java
+++ patterns/src/com/lea/pattern_1/Main.java
@@ -1,4 +1,4 @@
-package com.lea.pattern_1.singleton;
+package com.lea.pattern_1;
 
 /**
  * @author lzc
@@ -6,6 +6,10 @@ package com.lea.pattern_1.singleton;
  * 单例模式：
  *  Ensure a class has only one instance, and provide a global point of access to it;
  *  确保一个类只有一个实例，且自行实例化并向整个系统提供这个实例
+ *
+ *  优点：
+ *  缺点：
+ *  使用场景：一个对象
  */
 public class Main {
 }
